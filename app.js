3a6ca23822f17a403cbaf0ec9632fda104f607f0
sdorado2 <sdorado2@gmail.com>
2023-02-16 18:11:24 -0500

    restaurant mockup phase one

---
+++ app.js
@@ -0,0 +1,204 @@
+// name: {},
+// img: {},
+// text: {},
+
+let menu = {
+  morning: {
+    food: [
+      {
+        name: "Tamales",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+      {
+        name: "Huevos Revueltos",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+      {
+        name: "Guajolota",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      }
+    ],
+
+    drinks: [
+      {
+        name: "Champorado",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+      {
+        name: "Arroz con Leche",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+      {
+        name: "Cafe",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+    ],
+  },
+
+  lunch: {
+    food: [
+      {
+        name: "Tortas",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+      {
+        name: "Tacos",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+      {
+        name: "Pambazos",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+    ],
+
+    drinks: [
+      {
+        name: "Jarrito",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+      {
+        name: "Mundet",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+      {
+        name: "Sangria",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+    ],
+  },
+
+  dinner: {
+    food: [
+      {
+        name: "Pollo Con Mole",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+      {
+        name: "Pozole",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+      {
+        name: "Pipian",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+    ],
+    drinks: [
+      {
+        name: "Agua de Limon",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+      {
+        name: "Agua de Jamaica",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+      {
+        name: "Horchata",
+        img: "",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+      },
+    ],
+  },
+};
+
+// let holidayOffers = {
+//   diaDeReyes: {
+//     itemOne: {
+//       name: "Pan De Reyes",
+//       img: "",
+//       text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+//     },
+//     drinkOne: {
+//       name: "Champurado",
+//       img: "",
+//       text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+//     }
+//   },
+
+//   dosDeFeb: {
+//     itemOne: {
+//         name: "Tamales Oaxaqueños",
+//         img: "",
+//         text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+//       },
+//       drinkOne: {
+//         name: "Champurado",
+//         img: "",
+//         text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eum similique autem explicabo, praesentium molestiae! Nulla pariatur modi inventore praesentium incidunt porro odit rerum cum.",
+//       }
+//   },
+//   diaDelNino: {
+//     name: {},
+//     img: {},
+//     text: {},
+//   },
+//   cincoDeMayo: {
+//     name: {},
+//     img: {},
+//     text: {},
+//   },
+//   diaDeLasMadres: {
+//     name: {},
+//     img: {},
+//     text: {},
+//   },
+//   independenceDay: {
+//     name: {},
+//     img: {},
+//     text: {},
+//   },
+//   diaDeMuertos: {
+//     name: {},
+//     img: {},
+//     text: {},
+//   },
+//   doceDeDic: {
+//     name: {},
+//     img: {},
+//     text: {},
+//   },
+//   posadas: {
+//     name: {},
+//     img: {},
+//     text: {},
+//   },
+//   navidad: {
+//     name: {},
+//     img: {},
+//     text: {},
+//   },
+// };
+
+const itemList =()=>{
+    let fudList = document.querySelector('.foodList');
+
+    let boxOne = document.createElement('div');
+    boxOne.setAttribute('class', 'foodlist');
+
+    let foodImg = document.createElement('img');
+    foodImg.setAttribute('class', 'foodPic');
+
+    let foodName = document.createElement('div');
+    foodName.setAttribute('class', 'foodText');
+    foodName.innerHTML = menu.morning.food.name;
+
+    let foodDesc = document.createElement('div');
+    foodDesc.setAttribute('class', 'foodInfo');
+    food.innerHTML = menu.morning.food.text;
+}
